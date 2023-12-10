be38268c8fce6109783d88539218bf8cd0af9cca
jay-bulk <jay.bulk@proton.me>
2023-12-10 15:10:55 -0700

    fix: reformat packer

--- ruck.sh
+++ ruck.sh
@@ -1,5 +1,11 @@
+repo_dirs=(dev sandbox learn)
+npm_pks=()
 configs=(nvim gh bat iterm tmux)
 wdg=(.zshrc .zprofile)
+for i in ${!repo_dirs[@]}; do
+	dir=${repo_drs[$i]}
+	echo "checking repos under ${dir}"
+	# gh repo list ${org} --json name,latestRelease,visibility,lastUpdated | npx json | > ${file}.json
 for i in ${!configs[@]}; do
 	config=${configs[$i]}
 	echo "throwing ${config} into your rucksack..."
