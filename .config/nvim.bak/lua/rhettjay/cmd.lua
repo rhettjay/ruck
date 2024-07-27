b59c1099b78f0af18ff3890bd2e4bf348072ee04
Rhett Bulkley <jay.bulk@proton.me>
2024-07-27 02:27:51 -0600

    tool: old nvim

---
+++ .config/nvim.bak/lua/rhettjay/cmd.lua
@@ -0,0 +1,24 @@
+-- Turn off pase mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+	pattern = "*",
+	command = "set nopaste",
+})
+
+-- Fix conceallment level for json files
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "json", "jsonc" },
+	callback = function()
+		vim.wo.spell = false
+		vim.wo.concealleavel = 0
+	end,
+})
+
+-- Highlight on yank
+-- See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
+})
---
+++ .config/nvim.bak/lua/rhettjay/remap.lua
@@ -0,0 +1,56 @@
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- [[ Basic Keymaps ]]
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+-- Remap for dealing with word wrap
+keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+keymap.set("n", "dw", '"vb"_d')
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Jumplist
+keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- Split Window
+keymap.set("n", "sss", ":split<Return>", opts)
+keymap.set("n", "ssv", ":vsplit<Return>", opts)
+
+keymap.set("n", "<leader>ex", ":Neotree<CR>")
+
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- Resize Windows
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- Diagnostics -- set in ../plugins/lspconfig.lua
+--keymap.set("n", "<C-j>", function()
+--	vim.diagnostic.goto_next()
+-- end, opts)
+
+-- Highlight on yank
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
+})
