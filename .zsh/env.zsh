7bb30297e3bfb40fb4b9ccb999c08c7c1a815921
Rhett Bulkley <jay.bulk@tuta.io>
2024-07-26 21:00:58 -0600

    shell: move .zsh around

---
+++ .zsh/env.zsh
@@ -0,0 +1,85 @@
+# XDG (Cross Desktop Group) Environment
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$XDG_CONFIG_HOME/cache"
+
+# +-------------------------------------------------------------------------+
+# | zle options https://zsh.sourceforge.io/Doc/Release/Options.html#Options |
+# +-------------------------------------------------------------------------+
+bindkey '^j' history-search-backward
+bindkey '^k' history-search-forward
+
+# History
+HISTSIZE=5000
+HISTFILE=~/.zsh_history
+HISTFILESIZE=10000
+SAVEHIST=$HISTSIZE
+HISTDUPE=erase
+setopt extended_history
+setopt inc_append_history
+setopt append_history
+setopt hist_ignore_all_dups   # ignore duplicated commands history list
+setopt hist_ignore_dups       # ignore duplicated commands history list
+setopt hist_ignore_space      # ignore commands that start with space
+setopt hist_verify            # show command with history expansion to user before running it
+setopt auto_list
+
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Autosuggestions in terminal
+source "$HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+# autocomplete hidden files
+_comp_options+=(globdots)
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#946b2d,bold,underline"
+
+# Set up fzf key bindings and fuzzy completion
+source <(fzf --zsh)
+
+#+--------------+
+#| Path options |
+#+--------------+
+
+# Python 3.11
+# The original version is saved in .zprofile.pysave
+export PATH="/Library/Frameworks/Python.framework/Versions/3.11/bin:${PATH}"
+
+# unpack rucksack if not not built
+# [ -z stow "$RUCK" ]
+
+
+# VIM Environment
+export VIM="$XDG_CONFIG_HOME/nvim"
+export EDITOR="nvim"
+export VISUAL="nvim"
+
+# GPG Key
+export GPG_TTY=\$(tty)
+
+# Rust
+# Add .bin .cargo/bin /bin and wabt/build to bin
+export PATH="$PATH:$HOME/.bin:$HOME/.cargo/bin:$HOME/bin:$HOME/sandbox/web-assembly-crt/wabt/build:$HOME/.cargo/env"
+
+# WASM HOME
+export WASMTIME_HOME="$XDG_CONFIG_HOME/.wasmtime"
+export PATH="$WASMTIME_HOME/bin:$PATH"
+
+# VOLTA Setup
+export VOLTA_HOME="$XDG_CONFIG_HOME/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"
+
+
+# RANCHER Desktop
+export PATH="/Users/rbulkley/.rd/bin:$PATH"
+
+# NVM Setup (migrated to volta)
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# most important functions
+
+#+------------------+
+#| JQ Color options |
+#+------------------+
+#
+# Color Pattern: "null:false:true:numbers:strings:arrays:objects:object keys" [0;31:...]
+export JQ_COLORS="0;90:0;39:0;39:0;39:0;32:1;39:1;39:1;36"
---
+++ .zsh/opts.zsh
@@ -0,0 +1,151 @@
+# +--------+
+# | Colors |
+# +--------+
+default="\033[39m"
+black="\033[30m"
+red="\033[0;31m"
+green="\033[32m"
+yellow="\033[33m"
+blue="\033[34m"
+magenta="\033[35m"
+cyan="\033[36m"
+light_gray="\033[37m"
+dark_gray="\033[90m"
+light_red="\033[91m"
+light_green="\033[92m"
+light_yellow="\033[93m"
+light_blue="\033[94m"
+light_magenta="\033[95m"
+light_cyan="\033[96m"
+white="\033[97m"
+
+autoload -U colors && colors
+autoload -Uz vcs_info
+setopt prompt_subst
+
+# zstyle ':vcs_info:*' enable git
+# zstyle ':vcs_info:git:*' check-for-changes true
+## Add git info to zle
+
+# %r = repo name
+# %R = abosolute repo path
+# %s = vcs in use (git, svn, mercury)
+# %S = subdir within repo
+# %{%} = escaped sequence
+# %b = branch
+# %m = misc replacement (git rebase/cherry-pick) controlled by patch-format/no-patch-format
+# %u = The string from the unstagedstr style if there are unstaged changes in the repository.
+# %c = number of unapplied patches
+zstyle ':vcs_info:git:*' action-formats "%{$fg[cyan]%}%b%{$reset_color%} %m%u%c%{$reset_color%}%S"
+zstyle ':vcs_info:git:*' formats "%{$fg[cyan]%}%r %{$fg[red]%}%b%{$fg[cyan]%} %{$reset_color%}%m%u%c%{$reset_color%}%S"
+zstyle ':vcs_info:git:*' patch-formats "%{$fg[cyan]%}%b %{$reset_color%}%m%u%c%{$reset_color%}%S"
+
+# +--------+
+# | prompt |
+# +--------+
+#
+# setPrompt () {
+#   EMOJI="$*"
+#   DISPLAY_DIR='$(vcs_info)'
+#   DISPLAY_BRANCH='$(git_branch)'
+#   PROMPT='%~%{vcs_info_msg_0} ${vcs_info_msg_0_} ${EMOJI} '
+#   # Old Prompt included full directory listing
+#   #PROMPT='%u%~${vcs_info_msg_0_} ${EMOJI} '
+# }
+
+# https://unix.stackexchange.com/questions/287330/accessing-random-array-element-in-zsh
+# Randel picks a random element from the given list
+function randEL {
+ declare -a array=("$@")
+ r=$(($RANDOM % ${#array[@]}))
+ printf "%s\n" "${array[$r]}"
+ #return "${array[$r]}"
+}
+
+
+function setPromptID {
+  TERM_ID="$(randEL 🚀 👻 👾 🦄 🐳 🐻 🦊 🐙 🦖 🦕 🐢 🐉 ⚡️ 🔱 🦑)"
+}
+
+# function cd () {
+#   builtin cd "$@" && [[ -f .zsh_config ]] && . .aliases
+#   return 0
+# }
+
+dev_client="NOT SET"
+
+setPromptID
+
+precmd() {
+    vcs_info
+     if [[ -z $vcs_info_msg_0_ ]]; then
+       PROMPT="%3~ ${TERM_ID%2G%} "
+     elif [[ ! -z $vcs_info_msg_0_ ]]; then
+      if [[ $dev_client == "NOT SET" || -z $dev_gro ]]; then
+        PROMPT="$vcs_info_msg_0_ %F{yellow}e%f %{$TERM_ID%2G%} "
+      else
+        PROMPT="$vcs_info_msg_0_ %F{blue}L%f %{$TERM_ID%2G%} "
+      fi
+     fi
+}
+
+function calculateGitProjectHead {
+
+}
+
+function chpwd {
+  if [[ $(git $PWD rev-parse --is-inside-work-tree 2>/dev/null; echo $?) -ge 1 ]]; then
+      export dev_client="NOT SET"
+      export prd_client="NOT SET"
+    else
+      DIR="$(git rev-parse --show-toplevel --sq --quiet $PWD)"
+      if [[ $DIR != "" ]]; then
+        source "$DIR/.zsh_config"
+      else
+        return 1
+      fi
+  fi
+}
+
+
+#+----------------+
+#| Zstyle options |
+#+----------------+
+
+## Ignore .lock files wgedstr style if there are staged changes in the repository.hen using tab
+zstyle ':completion:*:*:vim:*:files' ignored-patterns '*.lock'
+## Add groups with autotab
+zstyle ':completion:*' group-name ''
+## pretty ls colors from tab
+zstyle ':completion:*' list-colors ${(s.:.)ZLS_COLORS}
+## ls -l from tab
+zstyle ':completion:*' files-list all
+## fuzzy find next best with tab
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+## cp file sort reverse
+zstyle ':completion:*:*:cp:*' file-sort modification reverse
+## menu
+zstyle ':completion:*' menu select
+##
+zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'style ':completion:*' menu select
+
+#eval "$(op completion zsh)"; compdef _op op
+
+
+#+------------------+
+#| zle vim keybinds |
+#+------------------+
+
+## bindkeys for menu navigation
+# zmodload zsh/complist
+# bindkey -M menuselect 'h' vi-backward-char
+# bindkey -M menuselect 'k' vi-up-line-or-history
+# bindkey -M menuselect 'j' vi-down-line-or-history
+# bindkey -M menuselect 'l' vi-forward-char
+
+## zstyle ':completion:*:*:*:*:*' verbose yes
+
+# allow vim like editing in zle
+# autoload -Uz edit-command-line
+# zle -N edit-command-line
+# bindkey -M vicmd v edit-command-line
