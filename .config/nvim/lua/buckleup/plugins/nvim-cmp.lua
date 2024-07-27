4ccaafddc0e3b400b650fbd48af189b5cdc58391
Rhett Bulkley <jay.bulk@proton.me>
2024-07-27 02:27:56 -0600

    editor: language specific protocols

---
+++ .config/nvim/lua/buckleup/plugins/nvim-cmp.lua
@@ -0,0 +1,40 @@
+return {
+	-- Autocompletion
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	enabled = true,
+	version = false,
+	dependencies = {
+		"hrsh7th/cmp-buffer", -- Source for text in buffer
+		"hrsh7th/cmp-path", -- Source for relative files
+		"hrsh7th/cmp-nvim-lsp", -- lsp auto complete
+		"L3MON4D3/LuaSnip", -- Snippet engine
+		"saadparwaiz1/cmp_luasnip", -- Autocomplete engine
+		"rafamadriz/friendly-snippets", -- useful snippets
+	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+		require("luasnip.loaders.from_vscode").lazy_load()
+		cmp.setup({
+			completion = {
+				completeopt = "menu,menuone,preview,noselect",
+			},
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<Tab>"] = cmp.mapping.confirm({ select = false }),
+				["<C-j>"] = cmp.mapping.select_next_item(),
+				["<C-k>"] = cmp.mapping.select_prev_item(),
+			}),
+			sources = cmp.config.sources({
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "path" },
+			}),
+		})
+	end,
+}
