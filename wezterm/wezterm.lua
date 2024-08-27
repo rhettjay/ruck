7f07fb591defa4e4c0652f50e47cf615df3aadef
Rhett Bulkley <jay.bulk@proton.me>
2024-08-27 16:20:42 -0600

    sh: wezterm window colors

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,51 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
-local config = {}
+local gui = wezterm.gui
+
+local function get_appearance()
+  if gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+local function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Builtin Solarized Dark'
+  else
+    return 'Builtin Solarized Light'
+  end
+end
+
+
+local config = {
+  color_scheme = scheme_for_appearance(get_appearance()),
+  use_fancy_tab_bar = true,
+  hide_tab_bar_if_only_one_tab = true
+}
+
+config.window_frame = {
+  font = wezterm.font { family = 'Nerd Font', weight = 'Bold' },
+
+  -- The size of the font in the tab bar.
+  -- Default to 10.0 on Windows but 12.0 on other systems
+  font_size = 12.0,
+
+  -- The overall background color of the tab bar when
+  -- the window is focused
+  active_titlebar_bg = '#333333',
+
+  -- The overall background color of the tab bar when
+  -- the window is not focused
+  inactive_titlebar_bg = '#333333',
+}
+
+config.colors = {
+  tab_bar = {
+    -- The color of the inactive tab bar edge/divider
+    inactive_tab_edge = '#575757',
+  },
+}
 
 config.keys = {
 	{
