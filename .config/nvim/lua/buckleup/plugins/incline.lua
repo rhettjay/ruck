3f362ee3e06b58322a2ba29fddafc0abf26d7c44
Rhett Bulkley <rbulkley@byu.edu>
2025-01-17 10:34:28 -0700

    editor: nvim gruvbox

--- .config/nvim/lua/buckleup/plugins/incline.lua
+++ .config/nvim/lua/buckleup/plugins/incline.lua
@@ -23,7 +23,7 @@ return {
             ' ',
             { filename, gui = modified and 'bold,italic' or 'bold' },
             ' ',
-            guibg = '#44406e',
+            -- guibg = '#44406e',
           }
         end,
       }
