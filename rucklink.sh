4d9b03fdbd464563cd0b67565b9dbb77e7e6d985
Rhett Bulkley <rbulkley@byu.edu>
2025-05-27 14:06:14 -0600

    stow configuration

--- rucklink.sh
+++ rucklink.sh
@@ -1,14 +1,10 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 
-while IFS="" read -r pkg || [[ -n "$pkg" ]]; do
-  if [[ ! -d "$pkg" || ! -d ".config/$pkg" ]]; then
-    printf "Warning: No config for %s \n" $pkg
-    exit 1
-  fi
-  if [[ "$pkg" == "zsh" ]]; then
-    printf "Linking .zsh\n"
-    ln -s .zshrc ~/.zshrc && \
-    ln -s .zsh/ ~/.zsh
-  fi
-  ln -s "./.config/$pkg" "~/.config/"
-done < ruck.conf
+pushd $DOTFILES
+for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
+do
+    echo "stow $folder"
+    stow -D $folder
+    stow $folder
+done
+popd
