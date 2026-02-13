cff42d5c5ab060f54402584d5840ae7026382ea2
Rhett Bulkley <rbulkley@byu.edu>
2026-02-13 13:05:25 -0700

    fix cloaking shield

--- .config/nvim/lua/buckleup/plugins/cloak.lua
+++ .config/nvim/lua/buckleup/plugins/cloak.lua
@@ -19,10 +19,17 @@ return {
 					-- Match an equals sign and any character after it.
 					-- This can also be a table of patterns to cloak,
 					-- example: cloak_pattern = { ":.+", "-.+" } for yaml files.
-					cloak_pattern = {
-            '=.+',
-            {'^grant.*=(.+)'},
-            {'{^https|^http}:.+'},
+          cloak_pattern = {
+            -- foo="hithere" -> foo="*******"
+            { '(=)"[^"]*', replace = '%1"' },
+            -- foo='hithere' -> foo='*******'
+            { "(=)'[^']*", replace = "%1'" },
+            -- bar=not -> bar=***
+            -- IMPORTANT: exclude quotes so it won't match foo="..."
+            { '(=)[^#%s"\']+', replace = "%1" },
+            -- keep the rest if you want, but make patterns valid Lua patterns
+            { '^grant.*="[^"]*"', },          -- example: whole line match
+            { '^https?://.+', },              -- URLs (your {^https|^http}:.+ won't do what you think)
           },
 				},
 			},
